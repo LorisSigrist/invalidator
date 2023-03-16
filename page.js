6d6b96ea755f196ce276c1d8129eb2f95c356826
Loris Sigrist <sigrist@hasch.io>
2023-03-16 22:20:10 +0100

    Fixed multiple sub/unsub bug

--- page.js
+++ page.js
@@ -1,7 +1,5 @@
 import { invalidate, depends } from "./lib/index";
 
-const btn = document.getElementById("btn");
-if (!btn) throw new Error("No button found");
 
 const count_1 = document.getElementById("invalidations_1");
 if (!count_1) throw new Error("No count found");
@@ -11,6 +9,9 @@ if (!count_2) throw new Error("No count found");
 
 const capped_count = document.getElementById("capped_count");
 if (!capped_count) throw new Error("No count found");
+
+const doubled_count = document.getElementById("doubled_count");
+if (!doubled_count) throw new Error("No count found");
 
 depends("data", () => {
   count_1.textContent = String(Number(count_1.textContent) + 1);
@@ -29,6 +30,29 @@ const unsub_capped = depends("data", () => {
   capped_count.textContent = String(count + 1);
 });
 
+
+// This should be called twice, but the second time it should be a no-op
+const incrementDoubledCount = () => {
+  doubled_count.textContent = String(Number(doubled_count.textContent) + 1);
+}
+
+const unsub_doubled_1= depends("data", incrementDoubledCount);
+const unsub_doubled_2 =depends("data", incrementDoubledCount);
+
+
+
+const btn = document.getElementById("btn");
+if (!btn) throw new Error("No button found");
+
+
 btn.addEventListener("click", () => {
   invalidate("data");
+});
+
+
+const doubled_unsub_one_btn = document.getElementById("doubled_unsub_one_btn");
+if (!doubled_unsub_one_btn) throw new Error("No button found");
+
+doubled_unsub_one_btn.addEventListener("click", () => {
+  unsub_doubled_1();
 });
--- tests/single-tab.spec.js
+++ tests/single-tab.spec.js
@@ -32,3 +32,27 @@ test("Unsubscribing from a dependent works", async ({ page }) => {
   await page.getByTestId("btn").click();
   await expect(page.getByTestId("capped_count")).toHaveText("2");
 });
+
+test("If a function is used as a dependent multiple times, it should be called multiple times", async ({ page }) => {
+  await page.goto("/");
+
+  await expect(page.getByTestId("doubled_count")).toHaveText("0");
+  await page.getByTestId("btn").click();
+  await expect(page.getByTestId("doubled_count")).toHaveText("2");
+});
+
+test("If a function is used as a dependent multiple times, and unsubscribed from, it should be called the correct number of times", async ({ page }) => {
+  await page.goto("/");
+
+  await expect(page.getByTestId("doubled_count")).toHaveText("0");
+  await page.getByTestId("btn").click();
+  await expect(page.getByTestId("doubled_count")).toHaveText("2");
+  await page.getByTestId("btn").click();
+  await expect(page.getByTestId("doubled_count")).toHaveText("4");
+
+  //Unsubscribe from one of the dependents
+  await page.getByTestId("doubled_unsub_one_btn").click();
+
+  await page.getByTestId("btn").click(); //This should only increment the count by 1
+  await expect(page.getByTestId("doubled_count")).toHaveText("5");
+});
