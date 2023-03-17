a73e0d5204d92598873fab753303957aaffeeca7
Loris Sigrist <sigrist@hasch.io>
2023-03-17 11:05:13 +0100

    Began implementing

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -3,7 +3,8 @@
 export function invalidate(key: string) : void
 
 /** 
- * Registers a callback that gets called whenever `invalidate` gets called with the same key 
+ * Registers a callback that gets called whenever `invalidate` gets called with the same key
+ * @param key The key to listen for, or a function that takes a key and returns true/false if this dependency matches
  * @returns An unsubscribe function
 */
-export function depends(key: string, callback: () => any ) : () => void
+export function depends(matcher_or_key: string | ((key: string) => boolean), callback: () => any ) : () => void
